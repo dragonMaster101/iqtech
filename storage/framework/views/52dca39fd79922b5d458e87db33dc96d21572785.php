a01c6a0123fac78a318d03891c69974fac083f27
eju <a.humeidh@gmail.com>
2017-04-04 18:21:46 +0800

    all project files commit

---
+++ storage/framework/views/52dca39fd79922b5d458e87db33dc96d21572785.php
@@ -0,0 +1,53 @@
+<?php $__env->startSection('content'); ?>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <!--  <h1 class="brand-heading">IQ Tech Sdn Bhd</h1>-->
+                        <p class="iqlogo animated-fast fadeIn"><img src="img/iq.jpg"/></p>
+                        <p class="intro-text animated-slow fadeIn">"IQ Tech's official website is under construction"</p>
+                        <a href="#contact" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contact IQ Tech</h2>
+                <p>IQ Tech Sdn Bhd provides a wide range of telecommunication services. Although we are under maintenance right now, you may contact
+                    <a href="mailto:info@iq.com.my">info@iq.com.my</a> for any further information required</p>
+                <p class="center">Thank you</p>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+
+
+    <!-- Theme JavaScript -->
+    <script src="js/grayscale.min.js"></script>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/551647eea7b175a67e1d804e611405032c8d8dbe.php
@@ -0,0 +1,15 @@
+<?php $__env->startSection('content'); ?>
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contact IQ Tech</h2>
+                <p>IQ Tech Sdn Bhd provides a wide range of telecommunication services. Although we are under maintenance right now, you may contact
+                    <a href="mailto:info@iq.com.my">info@iq.com.my</a> for any further information required</p>
+                <p class="center">Thank you</p>
+            </div>
+        </div>
+    </section>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
